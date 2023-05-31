1e1bd61a39276addf9f6721fd07bfd278cd71843
schmijul <julian.schmidt2@mailbox.tu-dresden.de>
2023-05-31 15:55:27 +0200

    train in batches

--- ho1/task1.py
+++ ho1/task1.py
@@ -1,7 +1,9 @@
+import os
+import time
+
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
 
        
 class LinearLayer:
@@ -50,70 +52,92 @@ def plot_results(x, y_true, y_pred):
 
     plt.show()
 
-def train_network(x_train, y_train, layer1, layer2, init_lr, epochs, target_loss=None):
-    lr = init_lr
+def train_network(x_train, y_train, layer1, layer2, init_learning_rate, epochs, batch_size, target_loss=None):
+    current_learning_rate = init_learning_rate
     best_loss = np.inf
     best_weights = None
     best_biases = None
     losses = []
-    last_lr_checkpoint = 0
+    last_learning_rate_checkpoint = 0
+
+    num_samples = x_train.shape[0]
+    num_batches = num_samples // batch_size
+
+    start_time = time.time()
+
     for epoch in range(epochs):
-        # Forward pass
-        layer1_outputs = sigmoid(layer1.forward(x_train))
-        layer2_outputs = layer2.forward(layer1_outputs)
+        # Shuffle the training data
+        indices = np.random.permutation(num_samples)
+        x_train = x_train[indices]
+        y_train = y_train[indices]
 
-        y_pred = layer2_outputs       
-        
-        # Compute the loss
-        loss = lossfct(y_pred,y_train)
-        losses.append(loss)
+        # Iterate over batches
+        for batch in range(num_batches):
+            start = batch * batch_size
+            end = start + batch_size
+            x_batch = x_train[start:end]
+            y_batch = y_train[start:end]
 
-        if epoch % 1000 == 0:
-            print(f"Epoch {epoch},Current loss: {np.round(loss,6)}, Best loss: {np.round(best_loss,6)}, Learning rate: {lr}")
+            # Forward pass
+            layer1_outputs = sigmoid(layer1.forward(x_batch))
+            layer2_outputs = layer2.forward(layer1_outputs)
+
+            y_pred = layer2_outputs
+
+            # Compute the loss
+            loss = lossfct(y_pred, y_batch)
+            losses.append(loss)
+
+            # If the loss is less than the best loss, update the best loss and save the weights and biases
+            if loss < best_loss:
+                best_loss = loss
+                best_weights = {"layer_1_weights": layer1.weights.copy(), "layer_2_weights": layer2.weights.copy()}
+                best_biases = {"layer_1_biases": layer1.biases.copy(), "layer_2_biases": layer2.biases.copy()}
+
             
-            
-        if (epoch % 3000 == 0) and (epoch != 0):
-            # If the loss has not decreased for 3000 epochs, decrease the learning rate
-            if np.min(losses[last_lr_checkpoint:epoch]) <= best_loss:
-                last_lr_checkpoint = epoch
-                lr *= 0.09
-                print(f"Learning rate decreased to {lr}")
-                                    
-        # If the loss is less than the best loss, update the best loss and save the weights and biases
-        if loss < best_loss:
-            best_loss = loss
-            best_weights = [layer1.weights.copy(), layer2.weights.copy()]
-            best_biases = [layer1.biases.copy(), layer2.biases.copy()]
-           
 
-        # If loss is less than the threshold, stop training
-        if target_loss:
-            if best_loss <= target_loss:
-                print(f"Training stopped at epoch {epoch}, loss: {loss}")
-                return best_weights, best_biases, losses
+            # Backward pass
+            d_layer2_outputs = 2 * (y_pred - y_batch) / batch_size
+            d_layer1_outputs = layer2.backward(d_layer2_outputs)
+            layer1.backward(sigmoid_derivative(layer1_outputs) * d_layer1_outputs)
 
-        # Backward pass
-        d_layer2_outputs = 2 * (y_pred - y_train) / y_train.shape[0]
-        d_layer1_outputs = layer2.backward(d_layer2_outputs)
-        layer1.backward(sigmoid_derivative(layer1_outputs) * d_layer1_outputs)
+            # Update the weights and biases
+            layer1.update(current_learning_rate)
+            layer2.update(current_learning_rate)
 
-        # Update the weights and biases
-        layer1.update(lr)
-        layer2.update(lr)
+            # If loss is less than the threshold, stop training
+            if target_loss:
+                if best_loss <= target_loss:
+                    print(f"Training stopped at epoch {epoch}, loss: {loss}")
+                    return best_weights, best_biases, losses
+
+            ##### Training Status Updates #####
+
+            if epoch % 1000 == 0 and batch == 0:
+                    print(f"Epoch {epoch}, Best loss: {best_loss}, Learning rate: {current_learning_rate}, Time[min]: {(time.time() - start_time) / 60}")
+                    
+
+            if (epoch % 3000 == 0) and (epoch != 0):
+                # If the loss has not decreased for 3000 epochs, decrease the learning rate
+                if np.min(losses[:epoch]) <= best_loss:
+                    print(f"Learning rate decreased from {current_learning_rate}to {current_learning_rate*0.1}")
+                    current_learning_rate *= 0.1
+
+
 
     return best_weights, best_biases, losses
 
-# Define the main function
+
 def main():
-    lr = 1e-1
+    N1 = 45
+    init_learning_rate = 0.1
     # Create a dataset
     x1 = np.arange(-5, 5, 0.1)
     x2 = np.arange(-5, 5, 0.1)
     x_train = np.array(np.meshgrid(x1, x2)).T.reshape(-1, 2)
     y_train = target_fct(x_train[:, 0], x_train[:, 1])
 
-    # Number of nodes in the hidden layer
-    N1 = 50
+    
 
     # Create the layers
     layer1 = LinearLayer(2, N1)
@@ -124,13 +148,14 @@ def main():
                                                       y_train=y_train, 
                                                       layer1=layer1,
                                                       layer2=layer2, 
-                                                      init_lr=lr, 
+                                                      init_learning_rate=init_learning_rate, 
                                                       epochs=100000,
+                                                      batch_size=300,
                                                       target_loss=0.002)
 
     # Use the best weights and biases to make predictions
-    layer1.weights, layer2.weights = best_weights
-    layer1.biases, layer2.biases = best_biases
+    layer1.weights, layer2.weights = best_weights["layer_1_weights"], best_weights["layer_2_weights"]
+    layer1.biases, layer2.biases = best_biases["layer_1_biases"], best_biases["layer_2_biases"]
     layer1_outputs = sigmoid(layer1.forward(x_train))
     layer2_outputs = layer2.forward(layer1_outputs)
 

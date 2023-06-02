26d8e1117af74551305107689708584fe575d63b
schmijul <julian.schmidt2@mailbox.tu-dresden.de>
2023-06-02 08:16:53 +0200

    backporpagation as own fct

--- ho1/task1.py
+++ ho1/task1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 class LinearLayer:
     def __init__(self, input_dim, output_dim):
@@ -44,18 +45,50 @@ def lossfct(y_hat, y_true):
     return np.sum((y_hat - y_true)**2) / y_true.shape[0]
 
 def plot_training_progress(training_progress):
-    plt.plot(training_progress)
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
+    #plt.plot(training_progress)
+    # plot with log scale
+    plt.semilogy(training_progress)
+    plt.xlabel("Epoch [linear scale]")
+    plt.ylabel("Loss [log scale]")
     plt.title("Training Progress")
     plt.show()
 
-def train(model, x_train, y_train, num_epochs, batch_size, init_lr=1,target_loss=None, scale_data=False, decay_lr=None, decay_interval=None, gradient_clip=None, verbose=False):
+def backpropagation(model, x, y, current_lr):
+    # Forward Pass
+    y_hat = model.forward(x)
+    loss = lossfct(y_hat, y)
+
+    # Backward Pass
+    d_loss = 2 * (y_hat - y) / y.shape[0]
+    grad_w2 = np.matmul(model.linear1.outputs.T, d_loss)
+    grad_b2 = np.sum(d_loss, axis=0)
+    d_sigmoid = sigmoid(model.linear1.outputs) * (1 - sigmoid(model.linear1.outputs))
+    grad_w1 = np.matmul(x.T, np.matmul(d_loss, model.linear2.weights.T) * d_sigmoid)
+    grad_b1 = np.sum(np.matmul(d_loss, model.linear2.weights.T) * d_sigmoid, axis=0)
+
+    # Gradient Clipping
+    gradient_clip = None  # Set gradient_clip if desired
+    if gradient_clip is not None:
+        grad_w2 = np.clip(grad_w2, -gradient_clip, gradient_clip)
+        grad_b2 = np.clip(grad_b2, -gradient_clip, gradient_clip)
+        grad_w1 = np.clip(grad_w1, -gradient_clip, gradient_clip)
+        grad_b1 = np.clip(grad_b1, -gradient_clip, gradient_clip)
+
+    # Update Parameters
+    model.linear2.weights -= current_lr * grad_w2
+    model.linear2.biases -= current_lr * grad_b2
+    model.linear1.weights -= current_lr * grad_w1
+    model.linear1.biases -= current_lr * grad_b1
+
+    return loss
+
+def train(model, x_train, y_train, num_epochs, init_lr=1, target_loss=None, scale_data=False, decay_lr=None, decay_interval=None, gradient_clip=None, verbose=False):
     current_lr = init_lr
     best_loss = np.inf
 
     training_progress = []
-    if scale_data:
+    start_time = time.time()
+    if (scale_data):
         y_min = np.min(y_train)
         y_max = np.max(y_train)
         y_train = (y_train - y_min) / (y_max - y_min)
@@ -65,74 +98,41 @@ def train(model, x_train, y_train, num_epochs, batch_size, init_lr=1,target_loss
         x_train = (x_train - x_min) / (x_max - x_min)
     for current_epoch in range(num_epochs):
 
-        # Shuffle the data
         idx = np.random.permutation(x_train.shape[0])
         x_train = x_train[idx]
         y_train = y_train[idx]
 
-        # Batch training
-        for batch_start in range(0, x_train.shape[0], batch_size):
-            batch_end = batch_start + batch_size
-            x_batch = x_train[batch_start:batch_end]
-            y_batch = y_train[batch_start:batch_end]
+        loss = backpropagation(model, x_train, y_train, current_lr)
+        training_progress.append(loss)
 
-            # Forward Pass
-            y_hat = model.forward(x_batch)
-            batch_loss = lossfct(y_hat, y_batch)
-            training_progress.append(batch_loss)
+        if (loss < best_loss):
+            best_loss = loss
+            no_improvement_counter = 0
+        else:
+            no_improvement_counter += 1
 
-            # Check if loss has improved
-            if batch_loss < best_loss:
-                best_loss = batch_loss
+        elapsed_time = time.time() - start_time
+
+        if ((decay_lr is not None) and (decay_interval is not None)):
+            if ((no_improvement_counter >= decay_interval) and ((current_epoch + 1) % decay_interval == 0)):
+                current_lr *= decay_lr
                 no_improvement_counter = 0
-            else:
-                no_improvement_counter += 1
 
-            # Decay learning rate if necessary
-            if decay_lr is not None and decay_interval is not None:
-                if no_improvement_counter >= decay_interval and (current_epoch + 1) % decay_interval == 0:
-                    current_lr *= decay_lr
-                    no_improvement_counter = 0
+        if (scale_data):
+            y_hat_rescaled = model.forward(x_train) * (y_max - y_min) + y_min
+            y_train_rescaled = y_train * (y_max - y_min) + y_min
+            loss = lossfct(y_hat_rescaled, y_train_rescaled)
 
-            # Backward Pass
-            d_loss = 2 * (y_hat - y_batch) / y_batch.shape[0]
-            grad_w2 = np.matmul(model.linear1.outputs.T, d_loss)
-            grad_b2 = np.sum(d_loss, axis=0)
-            d_sigmoid = sigmoid(model.linear1.outputs) * (1 - sigmoid(model.linear1.outputs))
-            grad_w1 = np.matmul(x_batch.T, np.matmul(d_loss, model.linear2.weights.T) * d_sigmoid)
-            grad_b1 = np.sum(np.matmul(d_loss, model.linear2.weights.T) * d_sigmoid, axis=0)
-
-            # Gradient Clipping
-            if gradient_clip is not None:
-                grad_w2 = np.clip(grad_w2, -gradient_clip, gradient_clip)
-                grad_b2 = np.clip(grad_b2, -gradient_clip, gradient_clip)
-                grad_w1 = np.clip(grad_w1, -gradient_clip, gradient_clip)
-                grad_b1 = np.clip(grad_b1, -gradient_clip, gradient_clip)
-
-            # Update Parameters
-            model.linear2.weights -= current_lr * grad_w2
-            model.linear2.biases -= current_lr * grad_b2
-            model.linear1.weights -= current_lr * grad_w1
-            model.linear1.biases -= current_lr * grad_b1
-
-        y_hat = model.forward(x_train)
-        loss = lossfct(y_hat, y_train)
-        if verbose:
-            if current_epoch % 10000 == 0:
-                if scale_data:
-                    y_hat_rescaled = y_hat * (y_max - y_min) + y_min
-                    y_train_rescaled = y_train * (y_max - y_min) + y_min
-                    loss = lossfct(y_hat_rescaled, y_train_rescaled)
-                print(f"Epoch: {current_epoch} Loss: {loss}")
-
-
-
-        # if Loss <=0.02 end training here
-
-        if (target_loss) and (loss <= target_loss):
-            print(f"reached target loss")
+        if ((target_loss) and (loss <= target_loss)):
+            print(f"Reached target loss after {current_epoch} epochs and {elapsed_time} seconds")
             return training_progress
+        
+        if (verbose):
+            if (current_epoch % 1000 == 0):  
+                print(f"Epoch: {current_epoch} Loss: {loss} Elapsed Time: {elapsed_time:.2f}s")
+
     return training_progress
+
 
 def plot_3d(x1, x2, y):
     fig = plt.figure(figsize=(12, 6))
@@ -140,11 +140,11 @@ def plot_3d(x1, x2, y):
     ax.scatter(x1, x2, y)
     plt.show()
 
-if __name__ == "__main__":
+if (__name__ == "__main__"):
     verbose = True
-    num_epochs = 100000
-    batch_size = 32
-    decay_lr = 0.9
+    num_epochs = 1e6
+    batch_size = 100
+    decay_lr = 0.1
     decay_interval = 1000
     scale_data = True
     init_lr = 0.1
@@ -159,7 +159,7 @@ if __name__ == "__main__":
     
     model = TwoLayerNet()
 
-    train_loss = train(model, x_train, y_train, num_epochs=num_epochs, batch_size=batch_size, init_lr=init_lr, scale_data=scale_data, verbose=verbose)
+    train_loss = train(model, x_train, y_train, num_epochs=num_epochs, init_lr=init_lr, target_loss=target_loss, scale_data=scale_data, verbose=verbose)
 
     plot_training_progress(train_loss)
     # save model state

4b9049157db1036c08c25f7d98bb62dc6784a821
schmijul <julian.schmidt2@mailbox.tu-dresden.de>
2023-06-30 09:55:37 +0200

    added img usage

--- task_5/convolution.py
+++ task_5/convolution.py
@@ -1,5 +1,7 @@
-import cv2
 import numpy as np
+from PIL import Image
+import cv2
+
 def convolution_plain_python(matrix, kernel):
     # Dimensions of the input matrix and the kernel
     m_rows, m_cols = len(matrix), len(matrix[0])
@@ -18,13 +20,14 @@ def convolution_plain_python(matrix, kernel):
 
     return output  
 
+
+
 def convoltuion_scipy(matrix, kernel):
     from scipy.signal import convolve2d
     return convolve2d(matrix, kernel, mode="valid")
 
 
 def maxpooling(matrix, dim):
-    # use numpy to apply maxpooling onto the matrix
     output = np.zeros((int(matrix.shape[0]/dim), int(matrix.shape[1]/dim)))
     for i in range(output.shape[0]):
         for j in range(output.shape[1]):
@@ -63,6 +66,29 @@ if __name__ == "__main__":
     Y1= maxpooling(np.array(X_conv_K1), 2)
 
 
+    print("X_conv_K1:\n ", np.array(X_conv_K1))
+    print("\n")
+    print("Y1: \n ", Y1)
 
 
+
+
+    # Load the image
+
+    img = np.array(Image.open("circle.png"))
+    import matplotlib.pyplot as plt
     
+    img_conv_k1 = convolution_plain_python(img, K1)
+
+    Y1 = maxpooling(np.array(img_conv_k1), 2)
+    
+
+    # show img, img_conv_k1, Y1 in one figure
+    fig, ax = plt.subplots(1, 3, figsize=(15, 15))
+    ax[0].imshow(img, cmap="gray")
+
+    ax[1].imshow(img_conv_k1, cmap="gray")
+
+    ax[2].imshow(Y1, cmap="gray")
+
+    plt.show()
